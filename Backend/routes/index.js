b19c9dffb312e50d2745adf4bc9ce6e3d3361cc3
Yao Xiao <Yao@wireless1x-155-41-90-44.bu.edu>
2015-09-22 20:16:02 -0400

    Version 2.0 Backend connected to Frontend

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// for database
 var mongoose = require('mongoose');
 var db = require('../config/db');
 var JitsiDashboard = db.JitsiDashboard;
 
+// for render .html file.
+var path = require("path");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jitsi Dashboard' });
+  // res.render('../views/index.ejs', { title: 'Jitsi Dashboard' });
+  res.sendFile(path.join(__dirname + '/../views/Frontend/dashboard.html'));
 });
 
 module.exports = router;
